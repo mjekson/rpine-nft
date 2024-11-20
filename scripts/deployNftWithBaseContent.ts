2326839081729171c01d451dd68718898e8eeb59
mitagmio <mitag.mio@gmail.com>
2024-11-20 14:00:45 +0000

    new collection

--- scripts/deployNftWithBaseContent.ts
+++ scripts/deployNftWithBaseContent.ts
@@ -2,11 +2,11 @@ import { Address } from '@ton/core';
 import { NetworkProvider } from '@ton/blueprint';
 import { NftCollectionV3 } from '../wrappers/NftCollectionV3';
 
-const COLLECTION_ADDRESS = Address.parse('EQA-YqAeZV_O1cTng5lkiyeVkRZ33lgj3P6byNDembCpBb9Z'); //testnet
+const COLLECTION_ADDRESS = Address.parse('EQDox8nQDCrnBYqAHyHd9wVI06UfBKU7c9aixcSr9kfxSDo-'); //testnet
 
 export async function run(provider: NetworkProvider) {
     const nftCollection = provider.open(NftCollectionV3.createFromAddress(COLLECTION_ADDRESS));
     await nftCollection.sendDeployNftWithBaseContent(provider.sender(), 
-        Address.parse('UQDkFMeWl29gxlimZkEBv4pbDVpNR2F4j5SArnJtVWqt_vdW') // to address
+        Address.parse('0QAULcjDZ4TK9huUxR4Vl_Tfa8JRooU3bhvPrmHJHZIPGY9d') // to address
     )
 }
