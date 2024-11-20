31020d1776db87950c7dc25539dce401804e56b8
mitagmio <mitag.mio@gmail.com>
2024-11-20 04:25:47 +0000

    buy testnet params

--- scripts/buy.ts
+++ scripts/buy.ts
@@ -2,15 +2,11 @@ import { Address, toNano } from '@ton/core';
 import { Proxy } from '../wrappers/Proxy';
 import { NetworkProvider } from '@ton/blueprint';
 
-const PROXY_ADDRESS = Address.parse('');
+const PROXY_ADDRESS = Address.parse('EQDk5MCKONMMwXSJc6ZmnfwVmyVgt4-qfvumIK2aUlesQhVP'); //testnet proxy
 export async function run(provider: NetworkProvider) {
     const proxy = provider.open(
         Proxy.createFromAddress(PROXY_ADDRESS)
     );
 
-    await proxy.sendBuy(provider.sender(), toNano('25'))
-
-    await provider.waitForDeploy(proxy.address);
-
-    // run methods on `proxy`
+    await proxy.sendBuy(provider.sender(), toNano('30'), provider.sender().address as Address);
 }
