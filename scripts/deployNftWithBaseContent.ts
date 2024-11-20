2a50be00a82bfad6200867f5d00e0840e1972bf3
mitagmio <mitag.mio@gmail.com>
2024-11-20 14:59:33 +0000

    new test

--- scripts/deployNftWithBaseContent.ts
+++ scripts/deployNftWithBaseContent.ts
@@ -2,7 +2,7 @@ import { Address } from '@ton/core';
 import { NetworkProvider } from '@ton/blueprint';
 import { NftCollectionV3 } from '../wrappers/NftCollectionV3';
 
-const COLLECTION_ADDRESS = Address.parse('EQDox8nQDCrnBYqAHyHd9wVI06UfBKU7c9aixcSr9kfxSDo-'); //testnet
+const COLLECTION_ADDRESS = Address.parse('EQAPkxIsDFz2sm1mQv6NIVghZD7HYmA_ld7wKtovtnMNZ9lq'); //testnet
 
 export async function run(provider: NetworkProvider) {
     const nftCollection = provider.open(NftCollectionV3.createFromAddress(COLLECTION_ADDRESS));
