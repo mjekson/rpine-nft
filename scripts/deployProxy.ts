838757c8fd433c1da4f401d547133aed5a408df1
mitagmio <mitag.mio@gmail.com>
2024-11-20 04:13:15 +0000

    upd deploy proxy param testnet

--- scripts/deployProxy.ts
+++ scripts/deployProxy.ts
@@ -2,7 +2,7 @@ import { Address, toNano } from '@ton/core';
 import { Proxy } from '../wrappers/Proxy';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
-const COLLECTION_ADDRESS = Address.parse('');
+const COLLECTION_ADDRESS = Address.parse('EQA-YqAeZV_O1cTng5lkiyeVkRZ33lgj3P6byNDembCpBb9Z'); //testnet
 export async function run(provider: NetworkProvider) {
     const proxy = provider.open(
         Proxy.createFromConfig(
@@ -10,7 +10,7 @@ export async function run(provider: NetworkProvider) {
                 price: toNano('30'),
                 refBack: toNano('4.5'),
                 collectionAddress: COLLECTION_ADDRESS,
-                adminAddress: Address.parse(''),
+                adminAddress: provider.sender().address as Address,
             }, await compile('Proxy')
         )
     );
