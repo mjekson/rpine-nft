984ff741dcf68d2b6f37f253700d09b9f7c26072
mitagmio <mitag.mio@gmail.com>
2024-11-20 04:18:56 +0000

    second owner testnet

--- scripts/setSecondOwnerNftCollectionV3.ts
+++ scripts/setSecondOwnerNftCollectionV3.ts
@@ -3,9 +3,9 @@ import { NetworkProvider } from '@ton/blueprint';
 import { NftCollectionV3 } from '../wrappers/NftCollectionV3';
 
 // Replace with your NFT collection contract address
-const COLLECTION_ADDRESS = Address.parse('');
+const COLLECTION_ADDRESS = Address.parse('EQA-YqAeZV_O1cTng5lkiyeVkRZ33lgj3P6byNDembCpBb9Z'); //testnet
 // Replace with the new second owner address
-const SECOND_OWNER_ADDRESS = Address.parse('');
+const SECOND_OWNER_ADDRESS = Address.parse('EQDk5MCKONMMwXSJc6ZmnfwVmyVgt4-qfvumIK2aUlesQhVP'); //testnet proxy
 
 export async function run(provider: NetworkProvider) {
     const nftCollection = provider.open(NftCollectionV3.createFromAddress(COLLECTION_ADDRESS)); 
